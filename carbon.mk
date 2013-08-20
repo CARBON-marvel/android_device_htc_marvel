a3fc348939ac2987a005069af8868d37c1dbd958
thehacka1 <kalakotig@gmail.com>
2013-08-20 19:22:13 -0400

    Carbonize cm.mk

--- carbon.mk
+++ carbon.mk
@@ -1,13 +1,13 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/marvel/device_marvel.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := marvel
-PRODUCT_NAME := cm_marvel
+PRODUCT_NAME := carbon_marvel
 PRODUCT_BRAND := htc_europe
 PRODUCT_MODEL := Wildfire S A510e
 PRODUCT_MANUFACTURER := HTC
@@ -16,12 +16,12 @@ PRODUCT_VERSION_DEVICE_RELEASE :=
 PRODUCT_VERSION_DEVICE_SPECIFIC := $(PRODUCT_VERSION_DEVICE_RELEASE)
 
 # CM_RELEASE := true
-CM_BUILDTYPE := OWLPROJECT
+CM_BUILDTYPE := Marvellious
 CM_EXTRAVERSION := $(PRODUCT_VERSION_DEVICE_RELEASE)
 
 
-BUILD_ID := CM10.1-$(shell date -u +%Y%m%d)
+BUILD_ID := Carbon-$(shell date -u +%Y%m%d)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_marvel BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_marvel/marvel:4.2.2/$(BUILD_ID)/v0.2:user/release-keys 
-PRODUCT_RELEASE_NAME := OWLPROJECT-Predator-(Based-CRYPTOMILK)
+PRODUCT_RELEASE_NAME := Marvellous
